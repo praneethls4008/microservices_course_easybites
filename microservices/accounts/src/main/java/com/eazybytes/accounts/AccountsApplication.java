f8a9090204b612f06c5469dd52b00afaa5fcafa7
praneeth reddy <praneethls4008@gmail.com>
2026-02-25 22:57:12 +0530

    eureka server

--- microservices/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ microservices/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -9,13 +9,14 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 /*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
 @EnableJpaRepositories("com.eazybytes.accounts.repository")
 @EntityScan("com.eazybytes.accounts.model")*/
-
+@EnableFeignClients
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
---
+++ microservices/accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,16 @@
+package com.eazybytes.accounts.service.client;
+
+import com.eazybytes.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//eureka regstered name
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @GetMapping("/api/fetch")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam  String mobileNumber);
+
+}
