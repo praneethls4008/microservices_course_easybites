7332cc643547b32da17bd82a65890f61f5e74257
praneeth reddy <praneethls4008@gmail.com>
2026-03-01 20:52:32 +0530

    spring cloud gateway custom routing and filters

---
+++ microservices/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package com.eazybytes.gatewayserver.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route("accounts_route", r -> r
+                        .path("/accounts/**")
+                        .filters(f -> f.rewritePath("/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route("cards_route", r -> r
+                        .path("/cards/**")
+                        .filters(f -> f.rewritePath("/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .route("loans_route", r -> r
+                        .path("/loans/**")
+                        .filters(f -> f.rewritePath("/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .build();
+    }
+}
