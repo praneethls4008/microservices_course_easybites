432d9d1aa9efe9352c81fda2a887b2c053721974
praneeth reddy <praneethls4008@gmail.com>
2026-03-22 20:44:54 +0530

    spring security: keycloak roles implementation

--- microservices/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ microservices/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.eazybytes.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -14,6 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security){
         security.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/loans/**").hasRole("LOANS")
+                        .pathMatchers("/cards/**").hasRole("CARDS")
                 .anyExchange()
                 .authenticated())
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
